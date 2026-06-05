ee5043864f1f32e5792f111d8d6147ada0ba118c
SamuelSaylor <samuelsaylor2025@gmail.com>
2026-06-04 23:00:13 -0500

    progress saving point

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ HEIGHT = 40
 OCTAVE = 4 # This is how "loud" the noise is basically think a music nnote and how they gotthat shape thing
 SCALE = 0.1
 
+PERMUTATION_TABLE = list(range(256))
+random.shuffle(PERMUTATION_TABLE)
+PERMUTATION_TABLE = PERMUTATION_TABLE+PERMUTATION_TABLE
+
 def fade_curve(t): return t * t * t * (t * (t * 6 - 15) + 10) # this is from ken perlins math equation
 
+# lerp(a,b,t) = a + 1 * (b-a) = (1-t)*a+t*b = a+t*(b-a)
+def lerp(a,b,t): return a+t*(b-a)
 
+#def get_distance_vector(x,y,dx,dy): return [x-dx,y-dy]
+
+#given a corner of the grid cell, how much does that corner "push" the noise value at point (x, y)?
+def gradiant_function(x,y):
+    distance_vector = get_distance_vector(x,y,)
 
 def terrain_generation(lacunarity, persistence):
     # loop through every row and column
